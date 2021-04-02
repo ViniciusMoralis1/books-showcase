834115d11efe04ae3838d27ae5e4b93eb9a40811
Vinicius Moralis <vinicius_moralis@hotmail.com>
2021-04-02 14:50:52 -0300

    configurando eslint, prettier e editorconfig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function App(){
+export default function App() {
   return (
     <div>
-      <h1>Inicio do desenvolvimento</h1>  
+      <h1>Inicio do desenvolvimento</h1>
     </div>
   );
-};
+}
